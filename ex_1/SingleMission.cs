34bfc627469350432a581125b8b85669c9aaeb81
okopel <okopel@gmail.com>
2019-03-10 23:50:11 +0200

    v1.0

---
+++ ex_1/SingleMission.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class SingleMission : IMission
+    {
+        private string name;
+        private string type;
+        public string Name
+        {
+            get {
+                return this.name;
+            }
+        }
+        
+        public string Type
+        {
+            get
+            {
+                return this.type;
+            }
+        }
+
+        private Function function;
+
+        public event EventHandler<double> OnCalculate; //todo
+
+        public SingleMission( Function function, string name)
+        {
+            this.name = name;
+            this.function = function;
+        }
+       
+        public double Calculate(double value)
+        {
+            return this.function(value);
+        }
+        
+        
+    }
+}
